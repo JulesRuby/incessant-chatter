d31a03601781b12161db9b36b47ab4575ea4dbeb
JulesRuby <JulesRuby@users.noreply.github.com>
2021-06-22 13:17:11 -0600

    router/index.js : added requireAuth route guard; Checks if user logged in when accessing chatroom, failure redirects to Welcome.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,26 @@
+// VUE IMPORT
 import { createRouter, createWebHistory } from 'vue-router'
+
+// FIRBASE IMPORTS
+import { chatAuth } from '@/firebase/config';
+
+// VIEWS
 import Welcome from '../views/Welcome.vue'
 import ChatRoom from '../views/ChatRoom.vue'
+
+
+// create an authentication route guard
+const checkAuth = (to, from, next) => {
+  let user = chatAuth.currentUser; // this is more efficient than using the composable, since we are only conerned with the current user
+
+  // if no user, redirect to home screen
+  if (!user) {
+    next({ name: 'Welcome' })
+  } else {
+    next(); // else enter destination
+  }
+
+}
 
 const routes = [
   {
@@ -11,7 +31,8 @@ const routes = [
   {
     path: '/chatroom',
     name: 'ChatRoom',
-    component: ChatRoom
+    component: ChatRoom,
+    beforeEnter: checkAuth
   },
 ]
 
