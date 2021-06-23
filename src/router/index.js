b645bba82071c476bedede534a7eb33bb6845265
JulesRuby <JulesRuby@users.noreply.github.com>
2021-06-23 16:00:07 -0600

    Added requireNoAuth route guard : Redirect authenticated users to the chatroom; prevents users already logged in from needed to navigate through the welcome screen before before going to chatroom.

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,51 @@
 // VUE IMPORT
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 // FIRBASE IMPORTS
 import { chatAuth } from '@/firebase/config';
 
 // VIEWS
-import Welcome from '../views/Welcome.vue'
-import ChatRoom from '../views/ChatRoom.vue'
+import Welcome from '../views/Welcome.vue';
+import ChatRoom from '../views/ChatRoom.vue';
 
-
-// create an authentication route guard
+// Redirect unauthenticated users to Welcome
 const checkAuth = (to, from, next) => {
-  let user = chatAuth.currentUser; // this is more efficient than using the composable, since we are only conerned with the current user
+    let user = chatAuth.currentUser; // this is more efficient than using the composable, since we are only conerned with the current user
+    if (!user) {
+        next({ name: 'Welcome' });
+    } else {
+        next();
+    }
+};
 
-  // if no user, redirect to home screen
-  if (!user) {
-    next({ name: 'Welcome' })
-  } else {
-    next(); // else enter destination
-  }
-
-}
+// redirect logged in users to the ChatRoom
+const requireNoAuth = (to, from, next) => {
+    let user = chatAuth.currentUser;
+    if (user) {
+        next({ name: 'ChatRoom' });
+    } else {
+        next(); // else enter destination
+    }
+};
 
 const routes = [
-  {
-    path: '/',
-    name: 'Welcome',
-    component: Welcome
-  },
-  {
-    path: '/chatroom',
-    name: 'ChatRoom',
-    component: ChatRoom,
-    beforeEnter: checkAuth
-  },
-]
+    {
+        path: '/',
+        name: 'Welcome',
+        component: Welcome,
+        beforeEnter: requireNoAuth,
+    },
+    {
+        path: '/chatroom',
+        name: 'ChatRoom',
+        component: ChatRoom,
+        beforeEnter: checkAuth,
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
 
-export default router
+export default router;
