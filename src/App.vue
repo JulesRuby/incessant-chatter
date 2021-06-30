3c3d3dbc4ffadf87c48a19940bee913d1f526199
JulesRuby <JulesRuby@users.noreply.github.com>
2021-06-30 13:31:52 -0600

    Styling main.css / App.vue : changed font family, toying with colors of containers; added lighter color with lower opacity.

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
     display: flex;
     justify-content: center;
 
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    /* font-family: Avenir, Helvetica, Arial, sans-serif; */
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 }
