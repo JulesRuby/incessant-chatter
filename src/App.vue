a7f5b354c5063c4789c32de8876a4d0c78e1d385
JulesRuby <JulesRuby@users.noreply.github.com>
2021-07-09 15:03:28 -0600

    Added cloak to App.vue : messed around with hiding the app and applying a loader until the app is fully created; barely noticeable honestly, but good practice and will do for now. May create an HTML-wide solution in the future, instead of just app-mount.

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,37 @@ export default {
 </script>
 
 <style>
+[v-cloak] {
+    position: relative;
+    height: 100vh;
+    width: 100vw;
+    background-color: var(--c2-a6);
+}
+
+[v-cloak]::before {
+    position: absolute;
+    content: '';
+
+    width: 4rem;
+    height: 4rem;
+
+    top: 50%;
+    left: 50%;
+
+    background: linear-gradient(
+        135deg,
+        var(--c1) 0%,
+        var(--c4) 33%,
+        var(--c1) 66%,
+        var(--c4)
+    );
+    background-size: 300% 300%;
+
+    border-radius: 50%;
+
+    animation: rotate-gradient 5s linear infinite;
+}
+
 #app {
     flex-grow: 1;
     display: flex;
@@ -54,4 +85,14 @@ export default {
         max-height: 100vh;
     }
 }
+
+@keyframes rotate-gradient {
+    0% {
+        background-position: 0% 0%;
+    }
+    100% {
+        background-position: 100% 100%;
+    }
+}
+
 </style>
